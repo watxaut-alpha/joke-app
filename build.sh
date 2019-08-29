27448c9e0469f957401077256ded382038ec31e4
Joan Heredia Rius <watxaut@gmail.com>
2019-08-29 20:08:11 +0200

    - version 0.2.0 - added build.sh for docker and cron

---
+++ build.sh
@@ -0,0 +1,13 @@
+
+# stop all containers
+docker stop $(shell docker ps -q -a)
+
+# pull last changes from master
+git checkout master
+git pull
+
+# build new docker image
+docker build -t jokes-app:0.2.0 .
+
+# Run new container
+docker run -it --rm --name jokes-app-docker jokes-app:0.2.0
