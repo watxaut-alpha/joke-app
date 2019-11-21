b0519fc3174ba97a10a43b5cdae7782096e2fae1
Joan Heredia Rius <watxaut@gmail.com>
2019-11-21 19:04:02 +0100

    Added author to get next and droped do_send

--- src/api/main.py
+++ src/api/main.py
@@ -330,7 +330,7 @@ async def joke_rating_put(user_rating: UserRating):
 
 @app.get("/jokes/next")
 async def get_5_next_jokes(current_user: auth.User = Depends(get_current_user)):
-    df = jokes.get_5_next_jokes_to_send().drop(["author", "rating", "tags", "created_at"], axis=1)
+    df = jokes.get_5_next_jokes_to_send().drop(["so_send", "rating", "tags", "created_at"], axis=1)
     s = df.to_dict(orient="index")
     return s
 
