b2ebd987e2674602ead596d70cb79bb3f5d3fa6e
Joan Heredia Rius <watxaut@gmail.com>
2019-12-11 18:01:25 +0100

    changed pointing endpoints to old api route in bot

--- src/bot/src/api.py
+++ src/bot/src/api.py
@@ -4,6 +4,7 @@ from requests.exceptions import Timeout
 
 from src.secret import HOST
 
+API_V_STR: str = "/api/v1"
 TIMEOUT = 1.5  # secs
 
 
@@ -19,51 +20,51 @@ def connect(func):
 
 @connect
 def add_user_telegram(user_id: str, first_name: str) -> requests.Response:
-    url = "{host}/users/telegram/add".format(host=HOST)
+    url = f"{HOST}{API_V_STR}/users/telegram/add"
     data = {"user_id": str(user_id), "first_name": first_name}
     return requests.post(url, json=data, timeout=TIMEOUT)
 
 
 @connect
 def get_random_joke() -> requests.Response:
-    url = "{host}/jokes/random".format(host=HOST)
+    url = f"{HOST}{API_V_STR}/jokes/random"
     return requests.get(url, timeout=TIMEOUT)
 
 
 @connect
 def insert_rating_joke(user_id: str, joke_id: int, f_rating: float) -> requests.Response:
-    url = "{host}/jokes/rating".format(host=HOST)
+    url = f"{HOST}{API_V_STR}/jokes/rating"
     data = {"user_id": user_id, "joke_id": joke_id, "rating": f_rating, "source": "telegram"}
     return requests.put(url, json=data, timeout=TIMEOUT)
 
 
 @connect
 def update_joke_validation(validated_joke_id: int, user_id: str, is_joke: bool) -> requests.Response:
-    url = "{host}/jokes/validate".format(host=HOST)
+    url = f"{HOST}{API_V_STR}/jokes/validate"
     data = {"joke_id": validated_joke_id, "user_id": str(user_id), "is_joke": is_joke}
     return requests.put(url, json=data, timeout=TIMEOUT)
 
 
 @connect
 def get_random_validation_joke() -> requests.Response:
-    url = "{host}/jokes/validate/random".format(host=HOST)
+    url = f"{HOST}{API_V_STR}/jokes/validate/random"
     return requests.get(url, timeout=TIMEOUT)
 
 
 @connect
 def get_untagged_joke() -> requests.Response:
-    url = "{host}/jokes/tags/random".format(host=HOST)
+    url = f"{HOST}{API_V_STR}/jokes/tags/random"
     return requests.get(url, timeout=TIMEOUT)
 
 
 @connect
 def get_tags() -> requests.Response:
-    url = "{host}/jokes/tags".format(host=HOST)
+    url = f"{HOST}{API_V_STR}/jokes/tags"
     return requests.get(url, timeout=TIMEOUT)
 
 
 @connect
 def tag_joke(joke_id, user_id, tag_id) -> requests.Response:
-    url = "{host}/jokes/tag".format(host=HOST)
+    url = f"{HOST}{API_V_STR}/jokes/tag"
     data = {"joke_id": joke_id, "user_id": user_id, "tag_id": tag_id}
     return requests.put(url, json=data, timeout=TIMEOUT)
