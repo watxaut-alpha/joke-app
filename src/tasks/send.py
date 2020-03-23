4f49199ad4505cd9c0740c9959e0659c3ed1bacc
Joan Heredia <joan.heredia@glovoapp.com>
2020-03-23 08:44:43 +0100

    Erased send tweet function twitter and added request for send joke

--- src/tasks/send.py
+++ src/tasks/send.py
@@ -1,68 +1,30 @@
 import logging
+import os
 
-import src.db.core as db
-import src.db.jokes as jokes
-import src.db.users as users
-import src.mail.core as mail
-import src.mail.smtp as smtp
-from src.config import MAILGUN_USER as USER, MAILGUN_PWD as PASSWORD
+import dotenv
+import requests
 
-import src.web.twitter.twitter as twitter
+dotenv.load_dotenv(r"../../config/env-api.env")
 
 
-def send_mail(is_debug):
-    logger = logging.getLogger(__name__)
+def get_token_from_api():
+    oauth_docs_user = os.getenv("OAUTH_DOCS_USER")
+    oauth_docs_pwd = os.getenv("OAUTH_DOCS_PWD")
 
-    host = "localhost"
-    conn = db.get_jokes_app_connection(host)
+    url = "https://watxaut.com/api/v1/token"
+    params = {"username": oauth_docs_user, "password": oauth_docs_pwd}
+    headers = {"accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"}
+    r = requests.post(url=url, data=params, headers=headers)
+    return r.json()["access_token"]
 
-    d_receivers = users.get_users_mail(is_debug).to_dict(orient="index")
 
-    # get a joke that is not sent previously
-    df_joke = jokes.get_joke_not_sent_by_pfm_already(conn, limit=1, sent_from="mail")
-    if df_joke.empty:  # no more jokes in the DB???
-        return smtp.send_mail_watxaut(USER, PASSWORD, "NO MORE JOKES IN DB!!!")
-
-    # else, there is still jokes so unpack
-    d_joke = df_joke.to_dict(orient="index")[0]
-
-    # add author if any
-    if d_joke["author"] not in [None, ""]:
-        d_joke["joke"] += "\n\nby {}".format(d_joke["author"])
-
-    d_joke["joke"] = d_joke["joke"].replace("\n", "<br>")  # replace \n with html
-
-    is_sent = mail.send_joke_mails(USER, PASSWORD, d_receivers, d_joke, provider="smtp")
-
-    if is_sent:
-        if not is_debug:
-            jokes.put_sent_joke_db(conn, d_joke["id"], sent_from="mail")
-        logger.info("Joke sent via mail with joke_id='{}'".format(d_joke["id"]))
+def send_joke_from_api():
+    url = "https://watxaut.com/api/v1/jokes/send"
+    token = get_token_from_api()
+    headers = {"accept": "application/json", "Content-Type": "application/json", "Authorization": f"Bearer {token}"}
+    r = requests.post(url, headers=headers)
+    if r:
+        return True
     else:
-        logger.error("Joke not sent! joke_id='{}'".format(d_joke["id"]))
+        return False
 
-    return is_sent
-
-
-def send_tweet():
-    logger = logging.getLogger("jokeBot")
-
-    host = "localhost"
-    conn = db.get_jokes_app_connection(host)
-    # get a joke that is not sent previously
-    df_joke = jokes.get_joke_not_sent_by_pfm_already(conn, limit=1, sent_from="twitter")
-
-    if df_joke.empty:  # no more jokes in the DB???
-        return smtp.send_mail_watxaut(USER, PASSWORD, "NO MORE JOKES IN DB!!!")
-
-    # else, there is still jokes so unpack
-    d_joke = df_joke.to_dict(orient="index")[0]
-    api = twitter.init_twitter_handler()
-    response = twitter.send_tweet(api, d_joke["joke"])
-    logger.info(f"Sent joke: '{d_joke['joke']}'")
-    if response:
-        jokes.put_sent_joke_db(conn, d_joke["id"], sent_from="twitter")
-        logger.info("Joke sent via mail with joke_id='{}'".format(d_joke["id"]))
-    else:
-        logger.error("Joke not sent! joke_id='{}'".format(d_joke["id"]))
-    return response
